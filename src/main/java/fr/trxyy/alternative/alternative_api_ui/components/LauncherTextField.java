3afc879c9aa8c89161618bed89f76ef6f6433c67
bleuwers <bastienleuwers2@gmail.com>
2021-04-09 22:17:55 +0200

    Alternative API modifié by Capdrake

---
+++ src/main/java/fr/trxyy/alternative/alternative_api_ui/components/LauncherTextField.java
@@ -0,0 +1,90 @@
+package fr.trxyy.alternative.alternative_api_ui.components;
+
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+/**
+ * @author Trxyy
+ */
+public class LauncherTextField extends TextField {
+
+	/**
+	 * The Constructor
+	 * @param root The Pane to add the field
+	 */
+	public LauncherTextField(Pane root) {
+		this.setSize(100, 30);
+		this.setPosition(0, 0);
+		root.getChildren().add(this);
+	}
+
+	/**
+	 * The Constructor
+	 * @param s The text by default
+	 * @param root The pane to add the field
+	 */
+	public LauncherTextField(String s, Pane root) {
+		this.setText(s);
+		this.setSize(100, 30);
+		this.setPosition(0, 0);
+		root.getChildren().add(this);
+	}
+
+	/**
+	 * The Constructor
+	 * @param w The width
+	 * @param h The height
+	 * @param pX The position X
+	 * @param pY The position Y
+	 * @param root The Pane to add the field
+	 */
+	public LauncherTextField(int w, int h, int pX, int pY, Pane root) {
+		this.setSize(w, h);
+		this.setPosition(pX, pY);
+		root.getChildren().add(this);
+	}
+
+	/**
+	 * The Constructor
+	 * @param s The text by default
+	 * @param w The width
+	 * @param h The height
+	 * @param pX The position X
+	 * @param pY The position Y
+	 * @param root The Pane to add the field
+	 */
+	public LauncherTextField(String s, int w, int h, int pX, int pY, Pane root) {
+		this.setText(s);
+		this.setSize(w, h);
+		this.setPosition(pX, pY);
+		root.getChildren().add(this);
+	}
+	
+	/**
+	 * Set the size of the field
+	 * @param width_ The width
+	 * @param height_ The height
+	 */
+	public void setSize(int width_, int height_) {
+		this.setPrefSize(width_, height_);
+	}
+
+	/**
+	 * Set the position of the field
+	 * @param posX The position X
+	 * @param posY The position Y
+	 */
+	public void setPosition(int posX, int posY) {
+		this.setLayoutX(posX);
+		this.setLayoutY(posY);
+	}
+	
+	/**
+	 * Set the text to display when field is void
+	 * @param s The text to display
+	 */
+	public void setVoidText(String s) {
+		this.setPromptText(s);
+	}
+
+}
